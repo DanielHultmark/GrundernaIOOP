3b4ae80309b432b1c8c449e0eb13c87e26d0232b
Daniel Hultmark <danielhultmark@gmail.com>
2025-09-28 11:12:44 +0200

    Komplett kod med kommentarer

---
+++ Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrundernaIOOP
+{
+    internal class Circle
+        //Skapade en klass
+    {
+        private double _radius;
+        //Skapade en konstruktor för radien
+        public Circle(double radius)
+        {
+            _radius = radius;            
+        }
+        //En metod för att räkna ut arean av cirkeln
+        public static double GetArea(double radius)
+        {            
+            return radius * radius * Math.PI;
+        }
+        
+
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace GrundernaIOOP
+﻿using System.Dynamic;
+
+namespace GrundernaIOOP
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Skapar 2st objekt för cirklarna av klassen Circle
+            Circle circle = new Circle(5);
+            Circle circle2 = new Circle(6);
+            //Hämtar metoden av cirkeln två gånger och sätter in 2st olika radier samt skriver ut själva cirkelns area
+            Console.WriteLine("Arean av cirkeln med 5 i radie är: " + Circle.GetArea(5));
+            Console.WriteLine("Arean av cirkeln med 6 i radie är: " + Circle.GetArea(6));
         }
     }
 }
